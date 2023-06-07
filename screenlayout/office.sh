2d1145fd5a7339ae422bc19da67c5fce3faa64be
Thiago Rocha <kimo@kimo.io>
2023-06-07 09:40:33 +0200

    Fixes multi-monitor setup

---
+++ screenlayout/office.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --primary --mode 1920x1200 --pos 2687x2160 --rotate normal --output HDMI-1 --off --output DP-1 --mode 3840x2160 --pos 1920x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --off
