36959f1dcf33dfa72474fe370eaddbaa6e3f1098
Thiago Rocha <kimo@kimo.io>
2023-07-12 10:10:54 +0200

    Handle polybar in multiple monitors. Still need to fix the order the workspaces of BSPWM appear in the polybars

--- scripts/load-polybar.sh
+++ scripts/load-polybar.sh
@@ -2,6 +2,12 @@
 
 killall -q polybar
 
-polybar example 2>%1 | tee -a /tmp/polybar.log & disown
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  MONITOR=$m polybar --reload example 2>%1 | tee -a /tmp/polybar.log & disown &
+done
+
+# polybar example 2>%1 | tee -a /tmp/polybar.log & disown
 
 echo "Polybar launched..."
