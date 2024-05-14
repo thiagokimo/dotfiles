c3289f565a7aa54adcaa4a244c81ca7021b98012
Thiago Rocha <kimo@kimo.io>
2024-05-14 12:59:24 -0300

    Create install-nix.sh

---
+++ scripts/nix/install-nix.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+
+git clone https://github.com/thiagokimo/dotfiles.git $HOME
+
+mkdir $HOME/home-manager
+cp $HOME/dotfiles/config/flake.nix $HOME/.config/home-manager
+cp $HOME/dotfiles/config/home.nix $HOME/.config/home-manager
+cp $HOME/dotfiles/config/flake.lock $HOME/.config/home-manager
