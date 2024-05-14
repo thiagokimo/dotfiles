3054aec372d6c363dc1386b0249ae0d616d653d1
Thiago Rocha <kimo@kimo.io>
2024-05-14 14:57:19 -0300

    Update install-nix.sh
    Fixes dotfiles path in home folder

--- scripts/nix/install-nix.sh
+++ scripts/nix/install-nix.sh
@@ -2,7 +2,7 @@
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
-git clone https://github.com/thiagokimo/dotfiles.git $HOME
+git clone https://github.com/thiagokimo/dotfiles.git $HOME/dotfiles
 
 mkdir -p $HOME/.config/home-manager
 cp $HOME/dotfiles/config/flake.nix $HOME/.config/home-manager
